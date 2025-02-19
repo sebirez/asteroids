7beb6dd3b115247e9e8fa3312d68f575dd272b45
sebirez <sar1906@rit.edu>
2025-02-19 15:25:49 -0600

    Added update function to rotate and move ship.

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pygame
 from constants import *
-black_rgb = (0, 0, 0)
+from player import *
 
 
 
 def main():
     pygame.init() 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+    game_clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+    
+    while True:                                                             #initiate game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
-    
+        pygame.Surface.fill(screen, (0, 0, 0))                              #draw black screen
+        player.draw(screen)                                                 #draw player
+        player.update(dt)                                                  #rotate left with "a", rotate right with "d"
+        
+        
+        pygame.display.flip()                                               #render screen and player
+        ms_next_frame = game_clock.tick(60)
+        dt = ms_next_frame/1000
     
     
     
